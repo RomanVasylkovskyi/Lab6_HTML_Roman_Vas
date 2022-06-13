a23433bf6f276d52e0ff68de1da06ecc3597c77f
RomanVasylkovskyi <101647171+RomanVasylkovskyi@users.noreply.github.com>
2022-06-14 01:22:00 +0300

    Add files via upload

---
+++ Lab6_HTML_Roman_Vas/js/index.js
@@ -0,0 +1,28 @@
+const fullpageApi = $.fn.fullpage;
+
+function getHash() {
+    return window.location.hash;
+}
+
+function setHash(value) {
+    window.location.hash = value;
+}
+$('.fullpage').fullpage({
+    anchors: ['Car', 'Car2', 'Car3']
+});
+$('.fullpage').fullpage({
+    anchors: ['firstPage', 'secondPage', 'thirdPage'],
+    lockAnchors: true,
+    onLeave: function(origin, destination, direction, trigger) {
+        $('.section[data-anchor="' + origin.anchor + '"]').removeClass('vis');
+        let destinationAnchor = $('[data-index="' + destination.index + '"]').attr('data-anchor');
+        setHash(destinationAnchor);
+    },
+    afterLoad: function(origin, destination, direction, trigger) {
+        $('.section[data-anchor="' + destination.anchor + '"]').addClass('vis');
+    }
+});
+
+$('[data-section]').on('click', function() {
+    fullpageApi.moveTo($(this).attr('data-section'));
+});
